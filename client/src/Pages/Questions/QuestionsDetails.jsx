63ddecddc40f05952f09e44d5feee6d717683039
unnath0 <timeandwind01@gmail.com>
2024-10-16 18:38:31 +0530

    changed url in question details page to use env variable

--- client/src/Pages/Questions/QuestionsDetails.jsx
+++ client/src/Pages/Questions/QuestionsDetails.jsx
@@ -24,7 +24,7 @@ const QuestionsDetails = () => {
   const dispatch = useDispatch();
   const User = useSelector((state) => state.currentUserReducer);
   const location = useLocation();
-  const url = "http://localhost:3000";
+  const url = process.env.REACT_APP_API_URL;
 
   const handlePostAns = (e, answerLength) => {
     e.preventDefault();
